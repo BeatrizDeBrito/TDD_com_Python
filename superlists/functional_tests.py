8c705a84ad7693d439d8cea64f47d4208105a01e
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2023-07-31 23:01:02 -0300

    Página inicial HTML agora é gerada de um template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,15 +12,16 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
     
     def test_can_start_a_list_and_retrieve_it_later(self):
+        browser = self.browser  # Fix 1: Use self.browser instead of browser
         browser.get('http://localhost:8000')
 
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').header_text
+        self.assertIn('To-Do', browser.title)  # Fix 2: Use self.browser instead of browser
+        header_text = browser.find_element_by_tag_name('h1').text  # Fix 3: Use text instead of header_text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribte('placeholder'),
+            inputbox.get_attribute('placeholder'),  # Fix 4: Use get_attribute instead of get_attribte
             'Enter a to-do item'
         )
         inputbox.send_keys('Buy peacock feathers')
@@ -28,11 +29,14 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
       
-        table = self.browser.find_element_by_id('id_lists_table')
-        rows = table.find_element_by_tag_name('tr')
+        table = browser.find_element_by_id('id_lists_table')
+        rows = table.find_elements_by_tag_name('tr')  # Fix 5: Use find_elements_by_tag_name instead of find_element_by_tag_name
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in the table"  # Fix 6: Corrected the typo in the message
         )
 
         self.fail('Finish the test!')
 
+if __name__ == '__main__':
+    unittest.main()
